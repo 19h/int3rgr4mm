0f706f7e2ddada261d76d5250266b7d650f3c788
Kenan Sulayman <kenan@sly.mn>
2013-10-30 12:18:58 +0100

    Implements listing
    Todo: fix the navigation to next day

--- int3r.js
+++ int3r.js
@@ -8,8 +8,6 @@ util = require("util"),
 fs = require("fs"),
 ez = require("echtzeit");
 
-!fs.existsSync("./logs") && fs.mkdirSync("./logs");
-
 var db = (function() {
 	var lru = {};
 
@@ -20,8 +18,8 @@ var db = (function() {
 	}
 })();
 
-var dhash = function () {
-	return ~~(Date.now()/86400000);
+var dhash = function (stamp) {
+	return ~~(((+stamp || Date.now())+((-(new Date).getTimezoneOffset())*60000))/86400000);
 };
 
 var spawn = function (dh, channel) {
@@ -39,6 +37,10 @@ var push = function ( plain, cb, cn ) {
 
 var read = function ( cn, dh, cb, end ) {
 	spawn(dh, cn).createVersionStream("").on("data", cb).on("end", end)
+};
+
+var read_bare = function ( cn, dh, cb, end, db, opts ) {
+	db.createReadStream(opts).on("data", cb).on("end", end)
 };
 
 var pshim = function ( plain, channel ) {
@@ -95,12 +97,37 @@ bot.addListener("error", console.log);
 var stamp = function ( ver ) {
 	var datum = new Date(ver/1000);
 
-	var h = datum.getHours(),
-	    m = datum.getMinutes(),
-	    s = datum.getSeconds();
+	var h = datum.getUTCHours(),
+	    m = datum.getUTCMinutes(),
+	    s = datum.getUTCSeconds();
 
 	return (h > 9 ? h : "0" + h) + ":" + (m > 9 ? m : "0" + m) + ":" + (s > 9 ? s : "0" + s)
 };
+
+var dstampFromDhash = function (entry) {
+	var d = new Date(entry * 86400000),
+	    h = d.getUTCFullYear(),
+	    m = d.getUTCMonth() + 1,
+	    s = d.getUTCDate();
+
+	return h + "-" + (m > 9 ? m : "0" + m) + "-" + (s > 9 ? s : "0" + s)
+}
+
+var dhashFromStamp = function (stamp) {
+	stamp = stamp.split("-").filter(function (e) { return !isNaN(e) });
+
+	// Number-Number-Number
+	if ( stamp.length !== 3 )
+		return false;
+
+	var stamp = new Date(stamp[0], stamp[1] - 1, stamp[2]);
+
+	// valid date
+	if ( isNaN( stamp.getTime() ) )
+		return false;
+
+	return dhash(stamp);
+}
 
 var transform = function( str ) {
 	str = str || "";
@@ -119,51 +146,106 @@ var transform = function( str ) {
 var _style = "<style type=\"text/css\">*{font-family:\"Segoe UI\", Arial, Helvetica, sans-serif;font-size:10pt}body,html{margin:0;padding:0}a:hover,a:active,a:focus{color:red}ul{padding:0 0 0 2em}div.navigation{background:#eee;height:1.5em;left:0;padding:.333em .666em;position:fixed;top:0;width:100%}div.navigation span.title{color:#444;font-weight:700}div.index,div.log{margin:2.5em 0 0;padding:0 .333em 1em}div.channels{margin:1.5em 0 0;padding:0 .333em 1em}table.log{border-collapse:collapse}table.log tr td{border:0;padding:.1em .5em;vertical-align:top}table.log tr td.time{border-right:1px solid #eee}table.log tr td.time a{color:#444;text-decoration:none}table.log tr td.time a.time-anchor{position:relative;top:-2.5em;visibility:hidden}table.log tr.kick td.content{color:red;font-style:italic}table.log tr.message td.content{color:#000}table.log tr.message td.content span.inverse{background-color:#000;color:#FFF}table.log tr.message td.content a.inverse{background-color:#000}table.log tr.message td.content .italic{font-style:italic}table.log tr.message td.content .monospace{font-family:monospace;white-space:pre}a,a:visited,table.log tr.message td.nick{color:#c50}div.navigation span.nolink,table.log tr.action td.nick,table.log tr.join td.nick,table.log tr.quit td.nick,table.log tr.part td.nick,table.log tr.kick td.nick,table.log tr.nick td.nick,table.log tr.topic td.nick{color:#444}table.log tr td.time a:hover,table.log tr.message td.content .underline{text-decoration:underline}table.log tr.action td.content,table.log tr.join td.content,table.log tr.quit td.content,table.log tr.part td.content,table.log tr.nick td.content,table.log tr.topic td.content{color:#444;font-style:italic}table.log tr.kick td.content span.victim,table.log tr.nick td.content span.new_nick,table.log tr.topic td.content span.topic,table.log tr.message td.content .bold{font-weight:700}</style>";
 
 var _render = function ( cn, dh, cb ) {
-	var d = new Date(dh * 86400000),
-	    h = d.getFullYear(),
-	    m = d.getMonth() + 1,
-	    s = d.getDate();
+	var dstamp = dstampFromDhash(dh);
 
-	var dstamp = (h > 9 ? h : "0" + h) + "-" + (m > 9 ? m : "0" + m) + "-" + (s > 9 ? s : "0" + s)
+	_index ( cn, function (exists) {
+		var dlisttarget, previous, next;
 
-	var _nav = "<div class=\"navigation\"><span class=\"title\">" + cn + " " + dstamp + "</span> | <a href=\"/\">index</a> | <a href=\"" + ( dh - 1 ) + "\">previous (" + dstamp + ")</a> | <span class=\"nolink\">next (none)</span> | <span class=\"nolink\">latest</span></div>";
-	var x = "<!DOCTYPE html><html lang=\"en\"><head><title>" + cn + " logs - " + dstamp + "</title>" + _style + "</head><body>" + _nav + "<div class=\"log\"><table class=\"log\"><tbody>";
-	var y = [];
+		// compute link to previous day
+		previous = !exists ? "<span class=\"nolink\">previous (none)</span>" : (dlisttarget = dstampFromDhash( dh - 1 ), previous = "<a href=\"" + dlisttarget + "\">previous (" + dlisttarget + ")</a>")
 
-	process.nextTick(function () {
-		read( cn, dhash(), function (a) {
-			var entry = JSON.parse(a.value),
-			    _stmp = stamp(a.version),
-			    _svbs = ~~(((a.version/1000)%1)*1000);
+		// compute link to next day
+		next = dh === dhash() ? "<span class=\"nolink\">next (none)</span>" : (dlisttarget = dlisttarget = dstampFromDhash( dh + 1 ), "<a href=\"" + dlisttarget + "\">next (" + dlisttarget + ")</a>")
 
-			var _e = "<tr class=\"" + entry.type + "\">";
-			    _e += "<td class=\"time\"><a href=\"#" + _stmp + "." + _svbs + "\">" + _stmp + "</a><a name=\"" + _stmp + "." + _svbs + "\" class=\"time-anchor\">&nbsp;</a></td>";
+		// compute link to latest
+		latest = dh === dhash() ? "<span class=\"nolink\">latest</span>" : "<a href=\"latest\">latest</a>";
 
-			if (~["part", "quit", "join"].indexOf(entry.type)) {
-				_e += "<td class=\"nick\">*  " + entry.target + "</td>"
-				
-				if ( entry.type !== "join" ) {
-					_e += "<td class=\"content\">" + entry.type + "<span class=\"reason\">" + (entry.payload ? " (" + transform(entry.payload) + ")" : "") + "</span></td></tr>";
+		var _nav = "<div class=\"navigation\"><span class=\"title\">" + cn + " " + dstamp + "</span> | <a href=\"/\">index</a> | " + previous + " | " + next + " | " + latest + "</div>";
+		var x = "<!DOCTYPE html><html lang=\"en\"><head><title>" + cn + " logs - " + dstamp + "</title>" + _style + "</head><body>" + _nav + "<div class=\"log\"><table class=\"log\"><tbody>";
+		var y = [];
+
+		process.nextTick(function () {
+			read( cn, dh, function (a) {
+				var entry = JSON.parse(a.value),
+				    _stmp = stamp(a.version),
+				    _svbs = ~~(((a.version/1000)%1)*1000);
+
+				var _e = "<tr class=\"" + entry.type + "\">";
+				    _e += "<td class=\"time\"><a href=\"#" + _stmp + "." + _svbs + "\">" + _stmp + "</a><a name=\"" + _stmp + "." + _svbs + "\" class=\"time-anchor\">&nbsp;</a></td>";
+
+				if (~["part", "quit", "join"].indexOf(entry.type)) {
+					_e += "<td class=\"nick\">*  " + entry.target + "</td>"
+					
+					if ( entry.type !== "join" ) {
+						_e += "<td class=\"content\">" + entry.type + "<span class=\"reason\">" + (entry.payload ? " (" + transform(entry.payload) + ")" : "") + "</span></td></tr>";
+					} else {
+						_e += "<td class=\"content\">joined</td></tr>";
+					}
+				} else if ( entry.type === "nick" ) {
+					_e += "<td class=\"nick\">" + entry.target + "</td>"
+					_e += "<td class=\"content\">changed nick to <span class=\"new_nick\">" + transform(entry.payload) + "</span></td></tr>";
 				} else {
-					_e += "<td class=\"content\">joined</td></tr>";
+					_e += "<td class=\"nick\">&lt;" + entry.target + "&gt;</td>"
+					_e += "<td class=\"content\">" + transform(entry.payload) + "</td></tr>";
 				}
-			} else if ( entry.type === "nick" ) {
-				_e += "<td class=\"nick\">" + entry.target + "</td>"
-				_e += "<td class=\"content\">changed nick to <span class=\"new_nick\">" + transform(entry.payload) + "</span></td></tr>";
-			} else {
-				_e += "<td class=\"nick\">&lt;" + entry.target + "&gt;</td>"
-				_e += "<td class=\"content\">" + transform(entry.payload) + "</td></tr>";
-			}
 
-			y.push(_e);
-		}, function () {
-			x += y.reverse().join("\n");
-			x += "</tbody></table></div></body><script src=\"client.js\"></script></html>";
-			
-			cb(x)
-		})
-	});
+				y.push(_e);
+			}, function () {
+				x += y.reverse().join("\n");
+				x += "</tbody></table></div></body><script src=\"client.js\"></script></html>";
+				
+				cb(x)
+			})
+		});
+	}, dh - 1);
 };
+
+var index_loop = function (start, cb, _db) {
+	var keys = [], currentKey = start;
+
+	var readordie = function (key) {
+		var hasData;
+
+		read_bare( void 0, void 0, function (a) {
+			currentKey = parseInt(a.key.split("\xFF")[1], 10),
+			hasData = true;
+
+			keys.push(currentKey);
+
+			setImmediate(function () {
+				readordie(++currentKey);
+			});
+		}, function () {
+			!hasData && cb(keys)
+		}, _db, { start: "\xFF" + key, limit: 1 })
+	}
+
+	readordie(currentKey);
+}
+
+var _index = function ( cn, _cb, exists ) {
+	var start, _db = db(cn), hasData, first = true;
+
+	// find first key
+	read_bare( void 0, void 0, function (a) {
+		hasData = true;
+
+		index_loop(parseInt(a.key.split("\xFF")[1], 10), function (keys) {
+			if (exists)
+				return _cb(~keys.indexOf(exists))
+
+			var nav = "<div class=\"navigation\"><span class=\"title\">" + cn + "</span> | <span class=\"nolink\">index</span> | <a href=\"latest\">latest</a></div>"
+
+			_cb(clogindex[0] + nav + clogindex[1] + keys.reverse().map(function (entry) {
+				var dstamp = dstampFromDhash(entry);
+
+				return "<li><a href=\"" + dstamp + "\">" + dstamp + "</a>" + (first && ((first = !1), " (<a href=\"latest\">latest</a>)") || "") + "</li>"
+			}).join("") + clogindex[2])
+		}, _db)
+	}, function () {
+		!hasData && _cb("") // error
+	}, _db, { start: "\xFF", limit: 1 })
+}
 
 var client = fs.readFileSync("./client.js"),
 lruc = {}, cindex = "";
@@ -178,6 +260,9 @@ cindex =  "<!DOCTYPE html><html lang=\"en\">" + _style + "<head><title>channel i
 			return "<li><a href=\"" + v + "/latest\">" + lruc[v] + "</a> (<a href=\"" + v + "/index\">index</a> | <a href=\"" + v + "/latest\">latest</a>)</li>"
 		}).join("")
 	+ "</ul></div></body></html>"
+
+clogindex = ["<!DOCTYPE html><html lang=\"en\">" + _style + "<head><title>channel index</title></head><body>", "<div class=\"index\"><ul>",
+		"</ul></div></body></html>"]
 
 var srv = http.createServer(function (request, response) {
 	if ( request.url === "/client.js" )
@@ -200,6 +285,27 @@ var srv = http.createServer(function (request, response) {
 			return _render( lruc[swap], dhash(), function (v) {
 				response.writeHead(200,{ "Connection": "yolo"}), response.end(v)
 			})
+		}
+
+		if ( (swap = request.url.substr(1, request.url.indexOf("/index") - 1)) in lruc ) {
+			return _index ( lruc[swap], function (v) {
+				response.writeHead(200,{ "Connection": "yolo"}), response.end(v)
+			});
+		}
+
+		if ( (swap = request.url.substr(1, request.url.substr(1).indexOf("/"))) in lruc ) {
+			var stamp;
+
+			if ( stamp = dhashFromStamp(request.url.substr(request.url.substr(1).indexOf("/") + 2)) ) {
+				return _index ( lruc[swap], function (exists) {
+					if ( exists )
+						return _render( lruc[swap], stamp, function (v) {
+							response.writeHead(200,{ "Connection": "yolo"}), response.end(v)
+						})
+
+					return response.end();
+				}, stamp)
+			}
 		}
 
 		return response.end();
